d9594a31f0836304ccff93b7bc70148c00eda396
Jon Kennedy <syrok77@gmail.com>
2015-05-23 00:44:56 -0700

    Working toolbar in OffenseActivity

--- app/src/main/java/com/paragonfervour/charactersheet/drawer/NavigationDrawerFragment.java
+++ app/src/main/java/com/paragonfervour/charactersheet/drawer/NavigationDrawerFragment.java
@@ -16,7 +16,6 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.google.inject.Inject;
 import com.paragonfervour.charactersheet.R;
@@ -261,16 +260,7 @@ public class NavigationDrawerFragment extends RoboFragment {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-
-        if (item.getItemId() == R.id.action_example) {
-            Toast.makeText(getActivity(), "Example action.", Toast.LENGTH_SHORT).show();
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return mDrawerToggle.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
     }
 
 //    private ActionBar getActionBar() {
@@ -280,7 +270,7 @@ public class NavigationDrawerFragment extends RoboFragment {
     /**
      * Callbacks interface that all activities using this fragment must implement.
      */
-    public static interface NavigationDrawerCallbacks {
+    public interface NavigationDrawerCallbacks {
         /**
          * Called when an item in the navigation drawer is selected.
          */
