84b0824fb03770220bf28feb84fc9d500cc0c300
Jon Kennedy <syrok77@gmail.com>
2015-05-26 22:41:38 -0700

    Fixing some weird thing that happened in the last commit.

---
+++ app/src/main/java/com/paragonfervour/charactersheet/fragment/BioFragment.java
@@ -0,0 +1,27 @@
+package com.paragonfervour.charactersheet.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.paragonfervour.charactersheet.R;
+
+import roboguice.fragment.RoboFragment;
+
+// TODO: This is a stub
+public class BioFragment extends RoboFragment {
+
+    public static BioFragment newInstance() {
+        return new BioFragment();
+    }
+
+    public BioFragment() {}
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_offense, container, false);
+    }
+}
